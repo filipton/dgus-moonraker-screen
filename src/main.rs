5474353baa9f8afecbab0dfb2b036e6bd8bd7933
filipton <filipton12@gmail.com>
2023-06-28 01:10:30 +0200

    feat: incoming data parsing

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,62 @@ fn main() -> Result<()> {
     }
 
     let mut serial = serial?;
+    serial.write(&construct_send_i16(0x2000, 69))?;
 
-    /*
     let mut buffer = vec![0; 100];
     loop {
         let len = serial.read(&mut buffer)?;
         if len > 0 {
             println!("Read {} bytes: {:#?}", len, &buffer[..len],);
-        }
-    }
-    */
 
-    let val = 69i16;
-    let data_length = val.to_be_bytes().len();
+            if buffer[3] == 0x82 {
+                println!("ACK");
+            } else if buffer[3] == 0x83 {
+                // read incoming data
+                let address = i16::from_be_bytes([buffer[4], buffer[5]]);
+                let data_length = buffer[2] - 4;
+
+                if data_length == 2 {
+                    let value = i16::from_be_bytes([buffer[7], buffer[8]]);
+                    println!("Address: {:#X} Value: {}", address, value);
+                } else {
+                    let value = std::str::from_utf8(&buffer[7..(7 + data_length as usize)])?;
+                    println!("Address: {:#X} Value: {}", address, value);
+                }
+            }
+        }
+
+    }
+
+    //Ok(())
+}
+
+fn construct_send_i16(address: i16, value: i16) -> Vec<u8> {
+    let data_length = value.to_be_bytes().len();
 
     let mut send_buff = vec![
-        0x5A, 0xA5, // Header
-        data_length as u8 + 4u8, // Data length
-        0x82, // Write
-        0x20, // Address
-        0x00, // Address
+        0x5A,                    // Header
+        0xA5,                    // Header
+        data_length as u8 + 3u8, // Length (+3 for address and command)
+        0x82,                    // Write
     ];
-    send_buff.extend_from_slice(&val.to_be_bytes());
+    send_buff.extend_from_slice(&address.to_be_bytes());
+    send_buff.extend_from_slice(&value.to_be_bytes());
 
-    // id why but sometimes i need to send data twice to change the value
-    serial.write(&send_buff)?;
+    send_buff
+}
 
-    Ok(())
+fn construct_send_text(address: i16, value: &str) -> Vec<u8> {
+    let data_length = value.len();
+
+    let mut send_buff = vec![
+        0x5A,                    // Header
+        0xA5,                    // Header
+        data_length as u8 + 3u8, // Length (+3 for address and command)
+        0x82,                    // Write
+    ];
+    send_buff.extend_from_slice(&address.to_be_bytes());
+    send_buff.extend_from_slice(value.as_bytes());
+
+    send_buff
 }
