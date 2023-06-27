41909c8ac743c503a4a81191a79b733fd84adfca
filipton <filipton12@gmail.com>
2023-06-27 22:49:44 +0200

    explore: sending the data

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() -> Result<()> {
 
     let mut serial = serial?;
 
+    /*
     let mut buffer = vec![0; 100];
     loop {
         let len = serial.read(&mut buffer)?;
@@ -16,4 +17,22 @@ fn main() -> Result<()> {
             println!("Read {} bytes: {:#?}", len, &buffer[..len],);
         }
     }
+    */
+
+    let val = 69i16;
+    let data_length = val.to_be_bytes().len();
+
+    let mut send_buff = vec![
+        0x5A, 0xA5, // Header
+        data_length as u8 + 4u8, // Data length
+        0x82, // Write
+        0x20, // Address
+        0x00, // Address
+    ];
+    send_buff.extend_from_slice(&val.to_be_bytes());
+
+    // id why but sometimes i need to send data twice to change the value
+    serial.write(&send_buff)?;
+
+    Ok(())
 }
