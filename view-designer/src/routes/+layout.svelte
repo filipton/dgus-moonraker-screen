67855abf1ef1a82832e214dd3488ae4b15bd07e0
filipton <filipton12@gmail.com>
2023-06-28 02:25:56 +0200

    feat: view designer skeleton

---
+++ view-designer/src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import "../app.css";
+    import html2canvas from "html2canvas";
+
+    async function save() {
+        //@ts-ignore
+        const canvas = await html2canvas(document.querySelector("#screen"), {
+            useCORS: true,
+        });
+        const a = document.createElement("a");
+        a.href = canvas.toDataURL("image/jpeg");
+        a.download = "screenshot.jpg";
+        a.click();
+    }
+</script>
+
+<div class="flex justify-center mt-8">
+    <div
+        class="border-lime-500 border-solid border-2"
+        style="width: 480px; height: 272px;"
+    >
+        <div id="screen" class="w-full h-full" style="background-color: black;">
+            <slot />
+        </div>
+    </div>
+</div>
+
+<div class="flex justify-center mt-2">
+    <button class="px-4 py-2 bg-gray-400" on:click={save}>save</button>
+</div>
