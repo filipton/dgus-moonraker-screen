e3e2fd4a4e6e6ce63b95e5eee26f77c526928e3f
filipton <filipton12@gmail.com>
2023-06-28 03:28:38 +0200

    feat: dynamic modules loading

--- view-designer/src/routes/+layout.svelte
+++ view-designer/src/routes/+layout.svelte
@@ -2,9 +2,33 @@
     import "../app.css";
     import html2canvas from "html2canvas";
 
+    let screen: HTMLDivElement;
+
+    const modules = import.meta.glob("./views/**.svelte");
+    let allModules: string[] = [];
+    let currentModule: string;
+
+    for (let module in modules) {
+        allModules.push(module);
+    }
+    currentModule = allModules[0];
+
+    async function prev() {
+        const index = allModules.indexOf(currentModule);
+        if (index > 0) {
+            currentModule = allModules[index - 1];
+        }
+    }
+
+    async function next() {
+        const index = allModules.indexOf(currentModule);
+        if (index < allModules.length - 1) {
+            currentModule = allModules[index + 1];
+        }
+    }
+
     async function save() {
-        //@ts-ignore
-        const canvas = await html2canvas(document.querySelector("#screen"), {
+        const canvas = await html2canvas(screen, {
             useCORS: true,
         });
         const a = document.createElement("a");
@@ -19,12 +43,37 @@
         class="border-lime-500 border-solid border-2"
         style="width: 480px; height: 272px;"
     >
+        <div
+            bind:this={screen}
+            class="w-full h-full"
+            style="background-color: black;"
+        >
+            {#await import(currentModule)}
+                <p>loading...</p>
+            {:then module}
+                <svelte:component this={module.default} />
+            {:catch}
+                <p>Error while loading module!</p>
+            {/await}
+        </div>
+    </div>
+</div>
+
+<!--
+<div class="flex justify-center mt-8">
+    <div
+        class="border-lime-500 border-solid border-2"
+        style="width: 480px; height: 272px;"
+    >
         <div id="screen" class="w-full h-full" style="background-color: black;">
             <slot />
         </div>
     </div>
 </div>
+-->
 
 <div class="flex justify-center mt-2">
-    <button class="px-4 py-2 bg-gray-400" on:click={save}>save</button>
+    <button class="px-4 py-2 bg-gray-400 mx-1" on:click={prev}>Prev</button>
+    <button class="px-4 py-2 bg-gray-400 mx-1" on:click={save}>Save</button>
+    <button class="px-4 py-2 bg-gray-400 mx-1" on:click={next}>Next</button>
 </div>
---
+++ view-designer/src/routes/views/test.svelte
@@ -0,0 +1,6 @@
+<img
+    src="https://odr.filipton.space/favicon.png"
+    class="h-2/3 mx-auto"
+    alt=""
+/>
+<h2 class="text-center text-4xl font-bold text-white my-auto">Connecting2</h2>
